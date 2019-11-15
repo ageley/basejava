17dc102ad58d9fce5012e803c7bbdd13e3e19837
ageley <57671769+ageley@users.noreply.github.com>
2019-11-15 14:36:54 +0300

    Первое задание basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,64 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int size = this.size();
+        if (r.uuid != null) {
+            storage[size] = r;
+        }
     }
 
     Resume get(String uuid) {
+        int size = this.size();
+        if (uuid != null) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = this.size();
+        if (uuid != null) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    for (int k = i; k < size; k++) {
+                        storage[k] = storage[k+1];
+                    }
+                    break;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = this.size();
+        Resume[] storageAll = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            storageAll[i] = storage[i];
+        }
+        return storageAll;
     }
 
     int size() {
-        return 0;
+        int size = storage.length;
+        for (int i = 0; i < size; i++) {
+            if (storage[i] == null) {
+                size = i;
+            }
+        }
+        return size;
     }
 }
