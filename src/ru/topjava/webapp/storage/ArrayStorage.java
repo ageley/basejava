84045f120f5635634e47220eb9e47455dcb50a66
ageley <57671769+ageley@users.noreply.github.com>
2020-01-10 16:35:37 +0300

    hotfix/lesson02
    + ArrayStorage
    - variables renamed for readability, removed unnecessary "this" keywords
    - storage amount is 10000
    - method clear - set to null only filled part of the storage
    - method getResumeIndex is private; removed check for null input, will be added in model class
    - method get - return null if uuid is not found
    + MainArray
    - variables renamed for readability
    - "update" method added
    + MainTestArrayStorage
    - variables renamed for readability

--- src/ru/topjava/webapp/storage/ArrayStorage.java
+++ src/ru/topjava/webapp/storage/ArrayStorage.java
@@ -8,38 +8,38 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[3];
+    private Resume[] storage = new Resume[10000];
     private int size = 0;
 
     public void clear() {
-        Arrays.fill(storage,null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void save(Resume r) {
-        int resumeIndex = this.getResumeIndex(r.getUuid());
+    public void save(Resume resume) {
+        int resumeIndex = getResumeIndex(resume.getUuid());
         if (resumeIndex >= 0) {
-            System.out.println(String.format("ERROR ru.topjava.webapp.storage.ArrayStorage.save: uuid '%1$s' is already exists", r.getUuid()));
+            System.out.println(String.format("ERROR ru.topjava.webapp.storage.ArrayStorage.save: uuid '%1$s' is already exists", resume.getUuid()));
         } else if (size >= storage.length) {
             System.out.println("ERROR ru.topjava.webapp.storage.ArrayStorage.save: storage is full");
         } else {
-            storage[size] = r;
+            storage[size] = resume;
             size++;
         }
     }
 
     public Resume get(String uuid) {
-        int resumeIndex = this.getResumeIndex(uuid);
+        int resumeIndex = getResumeIndex(uuid);
         if (resumeIndex < 0) {
             System.out.println(String.format("ERROR ru.topjava.webapp.storage.ArrayStorage.get: uuid '%1$s' is not exists", uuid));
+            return null;
         } else {
             return storage[resumeIndex];
         }
-        return null;
     }
 
     public void delete(String uuid) {
-        int resumeIndex = this.getResumeIndex(uuid);
+        int resumeIndex = getResumeIndex(uuid);
         if (resumeIndex < 0) {
             System.out.println(String.format("ERROR ru.topjava.webapp.storage.ArrayStorage.delete: uuid '%1$s' is not exists", uuid));
         } else {
@@ -60,23 +60,19 @@ public class ArrayStorage {
         return size;
     }
 
-    public void update(Resume r) {
-        int resumeIndex = this.getResumeIndex(r.getUuid());
+    public void update(Resume resume) {
+        int resumeIndex = getResumeIndex(resume.getUuid());
         if (resumeIndex < 0) {
-            System.out.println(String.format("ERROR ru.topjava.webapp.storage.ArrayStorage.update: uuid '%1$s' is not exists", r.getUuid()));
+            System.out.println(String.format("ERROR ru.topjava.webapp.storage.ArrayStorage.update: uuid '%1$s' is not exists", resume.getUuid()));
         } else {
-            storage[resumeIndex] = r;
+            storage[resumeIndex] = resume;
         }
     }
 
-    public int getResumeIndex(String uuid) {
-        if (uuid == null) {
-            System.out.println("ERROR ru.topjava.webapp.storage.ArrayStorage.getResumeIndex: uuid is null");
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    return i;
-                }
+    private int getResumeIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
             }
         }
         return -1;
